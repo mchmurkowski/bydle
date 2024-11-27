d505e6ea8439f41b3ec368eabae14df99a503b18
Michał Chmurkowski <4909359+mchmurkowski@users.noreply.github.com>
2024-11-27 12:05:06 +0100

    capitalize application window name correctly

--- src/bydle/gui.py
+++ src/bydle/gui.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 class Application(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title("Bydle")
+        self.title("ByDLe")
         self["padx"] = 20
         self["pady"] = 20
 
