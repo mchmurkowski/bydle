9553d447041e082cbb35f5e98631c24ce658681f
Michał Chmurkowski <4909359+mchmurkowski@users.noreply.github.com>
2024-11-27 11:55:38 +0100

    add --log command line flag to display log output in console

--- src/app.py
+++ src/app.py
@@ -1,11 +1,18 @@
+import argparse
 import logging
 
 from bydle.gui import Application, MainFrame
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--log", help="display log messages in the console", action="store_true"
+)
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    args = parser.parse_args()
+    if args.log:
+        logging.basicConfig(level=logging.INFO)
     app = Application()
     MainFrame(app)
     app.mainloop()
